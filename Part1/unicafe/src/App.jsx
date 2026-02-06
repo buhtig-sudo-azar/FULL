6fa6eb7b1de573ae505c1768fe31292e59e145bf
buhtig-sudo-azar <azarovvvit@mail.ru>
2026-02-06 18:50:29 +0300

    add color page

--- Part1/unicafe/src/App.jsx
+++ Part1/unicafe/src/App.jsx
@@ -2,43 +2,59 @@ import { useState } from 'react'
 import './App.css'
 
 
-const Button = (props)=>{
+const Button = (props) => {
 
-     const {name, onclick} = props;
+     const { name, onclick } = props;
 
      return (
-<>
+          <>
 
-<button onClick={onclick}>{name}</button>
-</>
+               <button onClick={onclick}>{name}</button>
+          </>
 
      )
 
 }
 
 function App() {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
- const clickHeandler = (setter) => ()=>{
-      setter(prev => prev + 1)
- }
-  return (
-    <>
-     <h4>Geeve feedback</h4>
+     const [isDarkMode, setIsDarkMode] = useState(false); // false = светлая, true = тёмная
+     const [good, setGood] = useState(0);
+     const [neutral, setNeutral] = useState(0);
+     const [bad, setBad] = useState(0);
 
-<Button onclick={clickHeandler(setGood)} name="good"/>
-<Button onclick={clickHeandler(setNeutral)} name="neutral"/>
-<Button onclick={clickHeandler(setBad)} name="bad"/>
+     const toggleThemeFactory = (setter) => () => {
+          setter(prev => !prev)
+     };
 
 
-<p>Sttatistics</p>
+     const clickHeandler = (setter) => () => {
+          setter(prev => prev + 1)
+     }
+     return (
+          <div style={{
+               backgroundColor: isDarkMode ? 'black' : 'white',
+               color: isDarkMode ? 'white' : 'black',
+               minHeight: '100vh'
+          }}>
+               <button onClick={toggleThemeFactory(setIsDarkMode)}>
+                    {isDarkMode ? '🌙 Тёмная тема' : '☀️ Светлая тема'}
+               </button>
+               <h4>Geeve feedback</h4>
 
-<p>{good}</p>
-<p>{neutral}</p>
-<p>{bad}</p>
-    </>
-  )
+               <Button onclick={clickHeandler(setGood)} name="good" />
+               <Button onclick={clickHeandler(setNeutral)} name="neutral" />
+               <Button onclick={clickHeandler(setBad)} name="bad" />
+
+
+               <p>Sttatistics</p>
+
+               <p>{good}</p>
+               <p>{neutral}</p>
+               <p>{bad}</p>
+
+
+          </div>
+     )
 }
 
 export default App
